fb91bb75580fbda1ed8a35bacff6c91a1dc0e186
Artem Krumin <trapper.programmer@gmail.com>
2022-06-23 20:03:54 +0300

    initializing git

---
+++ src/main/java/trapper/dev/spring_shopping_list/SpringShoppingListApplication.java
@@ -0,0 +1,13 @@
+package trapper.dev.spring_shopping_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShoppingListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShoppingListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/trapper/dev/spring_shopping_list/SpringShoppingListApplicationTests.java
@@ -0,0 +1,13 @@
+package trapper.dev.spring_shopping_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShoppingListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
